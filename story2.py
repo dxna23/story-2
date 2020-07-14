49296a488973b1615e17c887ae31a8c8085734b2
Chandana Akasapu <63527073+dxna23@users.noreply.github.com>
2020-07-13 23:32:55 -0500

    Add files via upload

---
+++ story2.py
@@ -0,0 +1,7 @@
+personName = input("Enter a name: ")
+place = input("Enter a place: ")
+anObject = input("Enter an object: ")
+anotherObject = input("Enter an object: ")
+
+story = personName + " was walking to " + place + " one day when they saw a fluorescent green "+ anObject + " trying to eat a huge " + anotherObject + ". " + personName + " decided to take the bus to " + place + " next time."
+print (story)
